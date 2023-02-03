965c3adbbd751ff88f8fde0daa4cd52e3f18c27d
POselisaacjajaja <124391806+POselisaacjajaja@users.noreply.github.com>
2023-02-03 12:45:23 -0600

    Add files via upload

---
+++ EVA1_1_HOLA_MUNDO/src/eva1_1_hola_mundo/EVA1_1_HOLA_MUNDO.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_1_hola_mundo;
+
+/**
+ *
+ * @author isaaczapatto
+ */
+public class EVA1_1_HOLA_MUNDO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int x = 100;
+        System.out.println(x);
+    }
+    
+    public static void algo(){
+        
+        System.out.println(x);
+        
+    }
+    
+}
